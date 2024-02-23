3e3517d4ffb8e84e8b9ace358ebce3adb3ce70f5
Vfranco00 <victorfranco02@outlook.com>
2024-02-23 16:44:01 -0300

    Feat: JSON + HMTL Structure + Main.js

---
+++ HTML.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang = "en">
+    <head>
+        <meta charset="UTF-8"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+        <meta name="viewport" content="width=devide-width, initial-scale=1.0" />
+        <title>Gerador de Senhas Seguras</title>        
+    </head>
+    <body>
+        <main>
+            <h1>Gerador de Senhas Seguras</h1>
+            <button>Gerar senha</button>
+            <h2>...</h2>
+        </main>
+        <script type="module" src="./src/main.js"></script>
+    </body>
+</html>
