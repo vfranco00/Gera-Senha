3bd108c952a987f7c2155df62652e9d44b1ce5a1
Vfranco00 <victorfranco02@outlook.com>
2024-02-23 18:15:14 -0300

    Feat: Style.css + Main.js + Index.hmtl

--- src/main.js
+++ src/main.js
@@ -1,10 +1,29 @@
 import { nanoid } from "nanoid";
+import "./style.css";
 
 const passwordBtn = document.querySelector('button');
 const passwordDisplay = document.querySelector('h2');
+const copyPassword = document.querySelector('.password');
 
 passwordBtn.addEventListener('click', () => {
     passwordDisplay.innerHTML = nanoid();
 });
+
+copyPassword.addEventListener('click', () => {
+    // Cria um elemento de texto temporário
+    var tempInput = document.createElement("input");
+    // Define o valor do elemento de texto como o texto a ser copiado
+    tempInput.value = passwordDisplay.textContent;
+    // Adiciona o elemento temporário à página
+    document.body.appendChild(tempInput);
+    // Seleciona o texto dentro do elemento de texto
+    tempInput.select();
+    // Copia o texto para a área de transferência
+    document.execCommand("copy");
+    // Remove o elemento temporário da página
+    document.body.removeChild(tempInput);
+    alert("Password copied");
+})
+
 
 
