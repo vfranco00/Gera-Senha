800bad3810d690c773a2731e496e4416475b87a5
Victor Franco <victorfranco02@outlook.com>
2024-02-26 21:16:23 -0300

    Fix: Removing comments

--- src/main.js
+++ src/main.js
@@ -9,7 +9,6 @@ const passwordLgtNumber = document.querySelector('#numberPassword');
 
 passwordBtn.addEventListener('click', () => {
     var tempLength = passwordLgthRange.value;
-    // passwordLgtNumber = passwordLgthRange.value;
     console.log(tempLength);
     passwordDisplay.innerHTML = nanoid(tempLength);
 });
@@ -21,17 +20,11 @@ passwordLgthRange.addEventListener('click', () => {
 });
 
 copyPassword.addEventListener('click', () => {
-    // Cria um elemento de texto temporário
     var tempInput = document.createElement("input");
-    // Define o valor do elemento de texto como o texto a ser copiado
     tempInput.value = passwordDisplay.textContent;
-    // Adiciona o elemento temporário à página
     document.body.appendChild(tempInput);
-    // Seleciona o texto dentro do elemento de texto
     tempInput.select();
-    // Copia o texto para a área de transferência
     document.execCommand("copy");
-    // Remove o elemento temporário da página
     document.body.removeChild(tempInput);
     alert("Password copied");
 });
